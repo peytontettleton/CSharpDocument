86670c587306290cfae7ef8d73dbf181f74363f6
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-09-13 18:45:28 -0500

    Update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace Csharpdoc
         static void Main(string[] args)
         {
             string pathString2 = @"c:\users\admin\documents\GitHub\CSharpDocument";
+
+
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 }
