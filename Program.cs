ea39517ce2a8693aba36007b425b7e1cd9204d03
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-09-13 18:44:54 -0500

    Create

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharpdoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString2 = @"c:\users\admin\documents\GitHub\CSharpDocument";
+        }
+    }
+}
