e73febd03f2ad26c4467f6c146e61f684bac4b8d
peytontettleton <36649600+peytontettleton@users.noreply.github.com>
2018-09-13 22:30:16 -0500

    Finishing

--- Program.cs
+++ Program.cs
@@ -6,14 +6,27 @@ namespace Csharpdoc
     class Program
     {
         static void Main(string[] args)
-        {            
-             string thepath = String.Format("{0}{1}{2}","C:\users\admin\documents\GitHub\CSharpDocument",idTextBox.text,".txt");
+        {
+            Console.WriteLine("What would you like to name your file:");
+            string FileName = Console.ReadLine() + ".txt";
+            Console.WriteLine("Okay, {0} it is", FileName);
 
-             using(StreamWriter writer = new StreamWriter(thepath))
-             {
-             writer.WriteLine();
-             }
-             
+            Console.WriteLine("What would you like to put into the file:");
+            string FileContents = Console.ReadLine();
+
+            var dir = @"c:\users\admin\documents\GitHub\CSharpDocument\";  // folder location
+            if (Directory.Exists(dir)) // if it doesn't exist, create
+            { 
+                Directory.CreateDirectory(dir);
+
+                // use Path.Combine to combine 2 strings to a path
+                File.WriteAllText(Path.Combine(dir, FileName), FileContents);
+                Console.WriteLine("{0} was saved successfully", FileName);
+            }
+           
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
